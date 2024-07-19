fd9fe648d0e45b707f0f14ce5c7c768988f7f935
abdoulfataho <yamyaogofataho@gmail.com>
2024-07-19 08:29:56 -0400

    final work

---
+++ src/Monster.java
@@ -0,0 +1,14 @@
+public class Monster {
+    public String name;
+
+    public Monster() {
+    }
+
+
+    public Monster (String name) {
+        this.name = name;
+    }
+    public String attack () {
+        return "!^_&^$@+%$* I don't know how to attack!";
+    }
+}
